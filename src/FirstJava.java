297a7ff28016ec8c49a1dae758d12cbe59ca3576
Sebastian Prodan <sebiprodan@gmail.com>
2018-01-03 21:51:05 +0200

    First Print Out

--- src/FirstJava.java
+++ src/FirstJava.java
@@ -1,3 +1,11 @@
+// HelloDate.java
+
+import java.util.*;
+
 public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("Hello it's: ");
+        System.out.println(new Date());
+    }
 
 }
