ce70092c40426b406109686d85d0d614a0000fb1
Sebastian Prodan <sebiprodan@gmail.com>
2018-01-05 15:50:25 +0200

    Light Class with the improved methods

--- src/Light.java
+++ src/Light.java
@@ -2,57 +2,49 @@ public class Light {
     public static void main (String[] args){
 
         double lightPower = Double.parseDouble(args[0]);
-        double dimPower = Double.parseDouble(args[1]);
-        double brightenPower = Double.parseDouble(args[1]);
+        double dimPower;
+        double brightenPower;
+        double lightPowerOn;
+        double lightPowerOf;
 
         Light firstLight = new Light();
 
-        //System.out.println(firstLight.on(lightPower));
-        //System.out.println(firstLight.off(lightPower));
-        System.out.println(firstLight.dim(lightPower,dimPower));
-        //System.out.println(firstLight.brighten(lightPower,brightenPower));
+        //System.out.println(firstLight.on());
+        //System.out.println(firstLight.off());
+        System.out.println(firstLight.dim(lightPower));
+        //System.out.println(firstLight.brighten(lightPower));
     }
 
-    public double on(double lightPower){
-        if (lightPower == 100) {
-            System.out.println("The light is on with its full power which is " + lightPower + "kWh");
+    public String on(){
+        String lightPowerOn = "The Light is ON";
+            return lightPowerOn;
         }
-        System.out.println("The current light power is: ");
-        return lightPower;
-    }
 
-    public double off(double lightPower){
-        if (lightPower == 0){
-            System.out.println("The light is of");
+    public String off(){
+        String lightPowerOf = "The Light is OFF";
+        return lightPowerOf;
         }
-        System.out.println("The current light power is: ");
-        return lightPower;
-    }
 
-    public double dim(double lightPower, double dimPower) {
-        double currentPower = lightPower;
-        lightPower = lightPower - dimPower;
-        if (lightPower > 0) {
-            System.out.println("The light power was dimed with " + dimPower + "kWh");
-            System.out.println("The current light power is: ");
-            return lightPower;
+    public double dim(double lightPower) {
+        double dimPower = lightPower - 10;
+        if (dimPower <= 0) {
+            System.out.println("The light is OFF");
         } else {
-            System.out.println("Please use switch off the light instead of dimming it. The light power will remain: ");
-            return currentPower;
-        }
+            System.out.println("The light power is now dimmed with 10 KwH");
+        }return dimPower;
     }
-    public double brighten(double lightPower, double brightenPower){
-        lightPower = lightPower + brightenPower;
-        if(lightPower > 100){
-            System.out.println("The light power is at its full power and can not be brighter");
-            System.out.println("The current light power will remain: ");
-            return lightPower - brightenPower;
-        }
-        else {
-            System.out.println("The light power was brightened  with " + brightenPower + "kWh");
-        }
-        System.out.println("The current light power is: ");
-        return lightPower;
+
+    public double brighten(double lightPower) {
+        double brightenPower = lightPower + 10;
+
+        if (brightenPower == 100) {
+            System.out.println("The light is now at its full power ");
+        } else if (brightenPower > 100){
+            System.out.println("The light power cannot be brighter than 100 KwH");
+            return lightPower;
+        }else
+            System.out.println("The light power is brighter with 10 KwH");
+        return brightenPower;
     }
 
 }
